3e2ff832696b328fe2a2de62411110980af4d047
teddybeargaming <160274508+teddybeargaming@users.noreply.github.com>
2024-02-20 16:33:35 +0000

    Source code

---
+++ Crackword.py
@@ -0,0 +1,6 @@
+password = None
+while password != "myPassword1234"
+password = input ("Enter the password.")
+if password != "myPassword1234"
+print ("Password incorrect! Try again.")
+print ("CONGRATULATIONS, YOU WIN!!!!!")
